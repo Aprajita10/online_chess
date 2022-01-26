3ee933175c0ee03655e4e6c82913b0d4f76f07e5
Aprajita10 <aprajita0132.cse19@chitkara.edu.in>
2022-01-26 21:38:11 +0530

    addfiles

---
+++ src/Square.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Square({children,isBlack}){
+
+    const bgClass=isBlack?'square-black':'square-white'
+    return (<div className={`${bgClass} board-square `}>
+    {children}
+</div>
+ )}
